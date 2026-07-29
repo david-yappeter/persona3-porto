9949bfa8821d6ce1ef58b3c8731a79fa3f42ece7
David Yappeter <davidyap11les@gmail.com>
2026-07-29 16:11:42 +0700

    feat: adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/persona3-porto/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/persona3-porto/' : '/',
   plugins: [react()],
-})
+}))
